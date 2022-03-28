f556720683efcba6ba392ad23248652913808203
HahaWu <1120991578@qq.com>
2022-03-28 16:50:30 +0800

    初始化提交

---
+++ src/main/java/com/sxdzsoft/easyresource/EasyresourceApplication.java
@@ -0,0 +1,13 @@
+package com.sxdzsoft.easyresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyresourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyresourceApplication.class, args);
+    }
+
+}
